dbcbd00828c13033e8e063af2f442adf50ea4061
rashidpkn <muhammed916rashid@gmail.com>
2026-09-21 17:05:25 +0530

    refactor: remove click handler from Luma Pay Portal button

--- src/components/DashboardSection.tsx
+++ src/components/DashboardSection.tsx
@@ -12,7 +12,7 @@ export const DashboardSection: React.FC = () => {
           <h2 className="h-large">
             <div className="anime-container lottie-anime eye">
               <svg viewBox="0 0 48 48" width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ display: 'inline-block', verticalAlign: 'middle', marginRight: '16px' }}>
-                <path d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15s18.54-6.22 22-15c-3.46-8.78-12-15-22-15zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6z" fill="currentColor"/>
+                <path d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15s18.54-6.22 22-15c-3.46-8.78-12-15-22-15zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6z" fill="currentColor" />
               </svg>
             </div>
             Increase your visibility in global spendings.
@@ -84,7 +84,6 @@ export const DashboardSection: React.FC = () => {
           <div className="btn-wrapper">
             <button
               type="button"
-              onClick={() => setIsAppModalOpen(true)}
               className="btn"
             >
               Access the Luma Pay Portal
